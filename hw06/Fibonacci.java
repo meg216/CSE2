571dc0b30fa405377a83bb483650bf0f19bad99d
meg216 <meg216@lehigh.edu>
2016-03-07 21:04:06 +0000

    added comments to Fibonacci

--- hw06/Fibonacci.java
+++ hw06/Fibonacci.java
@@ -14,23 +14,22 @@ public class Fibonacci{
         int a2=0;
         int n=0;
         int c=0;
-        int a=0;
-        boolean goodData = true;
         
-        System.out.print("Enter the first number of the sequence: ");
+        
+        System.out.print("Enter the first number of the sequence: "); //asking for the first number
         while(true){  
-            if(myscan.hasNextInt()){
-                a1=myscan.nextInt();
-                if(a1>=0){
+            if(myscan.hasNextInt()){  //checking that the value is an integer
+                a1=myscan.nextInt();  //assigning a1 if integer
+                if(a1>=0){             //if a1 is positive it can leave this loop and ask for the second number
                    break; 
                 }
-               else{
+               else{                        //if a1 is negative it will ask for a new input
                   
                    System.out.print("Enter the first number of the sequence: ");
                }
             }
             
-            else{ 
+            else{                   //asking for new input when a non integer is given
                 
                 System.out.print("Enter the first number of the sequence: ");
                 myscan.next();
@@ -40,7 +39,7 @@ public class Fibonacci{
                
         }
         
-       
+       //a2 and n follow same process as a1
         
         System.out.print("Enter the second number of the sequence: ");
         while(true){  
@@ -84,19 +83,19 @@ public class Fibonacci{
                
         }
          
-
+// all inputs have now been accepted as positive integers
       
-            System.out.print("The numbers are: " +a1+", ");
-            System.out.print(a2+", ");
-            n=n-2;
-        while (n>1){
-            c=a1+a2;
+            System.out.print("The numbers are: " +a1+", ");        
+            System.out.print(a2+", ");      //a1 and a2 are the first numbers of the sequence
+            n=n-2;          //since the first two have been printed n is now two less
+        while (n>1){        //doing fibonacci, n>1 because after that there needs to be a period instead of a comma
+            c=a1+a2;        
             a1=a2;
             a2=c;
             n=n-1;
             System.out.print(c+", ");
         }
-        if (n==1){
+        if (n==1){              //final number and the period
             c=a1+a2;
             System.out.println(c+ ".");
         
