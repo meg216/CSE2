ccdc44a78039e6dabf44bf520546e8b808f07ddb
meg216 <meg216@lehigh.edu>
2016-03-08 04:41:34 +0000

    updated TextSpinner

---
+++ hw06/TextSpinner.java
@@ -0,0 +1,25 @@
+// Maura Godfrey
+//cse 002
+//March 8, 2016
+//homework 6
+// Program 2- Text Spinner
+
+
+public class TextSpinner{
+    //main method required for all Java programs
+    public static void main(String[] args){
+        while(true){
+            System.out.print("\b"+ "/");   //deletes the previous part and prints each part of the spinner
+            System.out.print("\b"+ "-");
+            System.out.print("\b"+ "\\");
+            System.out.print("\b"+ "|");
+        }
+ 
+ 
+ 
+ 
+ 
+ 
+        
+    }
+}
