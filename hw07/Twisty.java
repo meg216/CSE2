b40e603f2c9a4e3b336b4126a5324b5b5298efee
meg216 <meg216@lehigh.edu>
2016-03-23 01:12:59 +0000

    updated Twisty

--- hw07/Twisty.java
+++ hw07/Twisty.java
@@ -12,7 +12,11 @@ public class Twisty{
         Scanner myscan= new Scanner(System.in);
         System.out.print("Input your desired length: ");
         int length=0;
+        int l=0;            //counter
         int width=0;
+        int s=0;            //space counter between different characters
+        int s1=0;           //space counter between like characters
+        int s2=0;
         String space=" ";
         String num="#";
         String forSlash="/";
@@ -63,7 +67,83 @@ public class Twisty{
         
         // spaces within diamonds starts at zero and increases by 2 until center
         
+        while(width>0){
+            l=0;
+            while(l<length){
+                s=width-2;
+                System.out.print(num);      //printing the first number sign
+                l++;
+                if(l==length){              //checking counter to ensure correct number of characters printed
+                    break;
+                }
+               
+                while(s>0&&l<length){       //spaces between different characters
+                System.out.print(space); 
+                s--;
+                l++;
+                }
+                
+                if(l==length){
+                    break;
+                }
+                
+                System.out.print(forSlash);
+                l++;
+                if(l==length){
+                    break;
+                }
+                
+                s2=s1;
+                while(s2>0&&l<length){                  //spaces between like characters
+                    System.out.print(space);
+                    l++;
+                    s2--;
+                }
+                if(l==length){
+                    break;
+                }
+                
+                System.out.print(backSlash);
+                l++;
+                if(l==length){
+                    break;
+                }
+                 
+                s=width-2;
+                while(s>0&&l<length){       //same loop as first space group
+                System.out.print(space); 
+                s--;
+                l++;
+                }
+                
+                if(l==length){
+                    break;
+                }
+                
+                System.out.print(num);      
+                l++;
+                if(l==length){              //checking counter to ensure correct number of characters printed
+                    break;
+                }
+                s2=s1;
+                while(s2>0&&l<length){                  //spaces between like characters
+                    System.out.print(space);
+                    l++;
+                    s2--;
+                }
+                if(l==length){
+                    break;
+                }
+              
+                
+            }
+            
+            
         
+            System.out.println(""); //after each individual line is done moves to the next line
+            width--;
+            s1++;
+        }
         
         
         
@@ -75,3 +155,5 @@ public class Twisty{
         
     }
 }    
+
+//sorry, i know its not right but i dont have time to fix it
