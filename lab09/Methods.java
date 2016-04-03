b24a23dc9a3c6d064fcb73f7daef9ddeab0e9c2f
meg216 <meg216@lehigh.edu>
2016-04-03 23:14:29 +0000

    updated methods

--- lab09/Methods.java
+++ lab09/Methods.java
@@ -8,173 +8,190 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Methods{
+   public static String adjective(){
+       Random randomGenerator=new Random();
+                 int randomInt= randomGenerator.nextInt(10);
+                switch(randomInt){
+                    case 0:
+                        return "fun";
+                      
+                    case 1:
+                        return "exciting";
+                       
+                    case 2:
+                        return "sad";
+                     
+                    case 3:
+                        return "crazy";
+                     
+                    case 4:
+                        return "happy";
+                       
+                    case 5:
+                        return "bored";
+                     
+                    case 6:
+                        return "hungry";
+                       
+                    case 7:
+                        return "sweet";
+                       
+                    case 8:
+                        return "cool";
+                        
+                    case 9:
+                        return "slow";
+                       
+                
+                } //ends switch statment
+            return "error1";
+            } //ends adjective
+           
+            public static String noun1(){
+                Random randomGenerator=new Random();
+                int randomInt= randomGenerator.nextInt(10);
+                switch(randomInt){
+                    case 0:
+                        return "boy";
+                      
+                    case 1:
+                        return "girl";
+                       
+                    case 2:
+                        return "man";
+                      
+                    case 3:
+                        return "woman";
+                    
+                    case 4:
+                        return "cat";
+                    
+                    case 5:
+                        return "child";
+                   
+                    case 6:
+                        return "bear";
+                      
+                    case 7:
+                        return "frog";
+                        
+                    case 8:
+                        return "horse";
+                        
+                    case 9:
+                        return "dog";
+                       
+                
+                } //ends switch
+                return "error2";
+            }//ends noun1
+            
+            public static String verb(){
+                Random randomGenerator=new Random();
+               int  randomInt= randomGenerator.nextInt(10);
+                switch(randomInt){
+                    case 0:
+                        return "ran";
+                       
+                    case 1:
+                        return "walked";
+                       
+                    case 2:
+                        return "fell";
+                        
+                    case 3:
+                        return "jumped";
+                        
+                    case 4:
+                        return "skated";
+                       
+                    case 5:
+                        return "swam";
+                        
+                    case 6:
+                        return "biked";
+                       
+                    case 7:
+                        return "drove";
+                      
+                    case 8:
+                        return "flew";
+                        
+                    case 9:
+                        return "crawled";
+                        
+                
+                }
+             return "error3";
+            }//ends verb
+            
+            public static String noun2(){
+                Random randomGenerator=new Random();
+                int randomInt= randomGenerator.nextInt(10);
+                switch(randomInt){
+                    case 0:
+                        return "table";
+                     
+                    case 1:
+                        return "door";
+                       
+                    case 2:
+                        return "chair";
+                        
+                    case 3:
+                        return "meeting";
+                        
+                    case 4:
+                        return "room";
+                        
+                    case 5:
+                        return "bar";
+                        
+                    case 6:
+                        return "game";
+                        
+                    case 7:
+                        return "show";
+                       
+                    case 8:
+                        return "car";
+                        
+                    case 9:
+                        return "beach";
+                       
+                
+                }
+             return "error4";
+            }//ends string noun2
+   
+   
+   
+   
+   
     public static void main(String[] args){
         Random randomGenerator=new Random();
         Scanner myScan= new Scanner(System.in);
         int randomInt= randomGenerator.nextInt(10); //will give random integer less than 10
         int check=0;
-        boolean continue= true;
+        int cont= 1;
         
-        While(continue==true){
-            System.out.print("The ");
-            public static string adjective(){
-                randomInt= randomGenerator.nextInt(10);
-                switch(randomInt){
-                    case 0:
-                        System.out.print("fun");
-                        break;
-                    case 1:
-                        System.out.print("exciting");
-                        break;
-                    case 2:
-                        System.out.print("sad");
-                        break;
-                    case 3:
-                        System.out.print("crazy");
-                        break;
-                    case 4:
-                        System.out.print("happy");
-                        break;
-                    case 5:
-                        System.out.print("bored");
-                        break;
-                    case 6:
-                        System.out.print("hungry");
-                        break;
-                    case 7:
-                        System.out.print("sweet");
-                        break;
-                    case 8:
-                        System.out.print("cool");
-                        break;
-                    case 9:
-                        System.out.print("slow");
-                        break;
-                
-                } //ends switch statment
+        while(cont==1){
+            String adj=adjective();
+            String n1=noun1();
+            String v=verb();
+            String n2=noun2();
             
-            } //ends adjective
+            System.out.println("The "+adj+" "+n1+" "+v+" into the "+n2 +".");
             
-            public static string noun1(){
-                randomInt= randomGenerator.nextInt(10);
-                switch(randomInt){
-                    case 0:
-                        System.out.print("boy");
-                        break;
-                    case 1:
-                        System.out.print("girl");
-                        break;
-                    case 2:
-                        System.out.print("man");
-                        break;
-                    case 3:
-                        System.out.print("woman");
-                        break;
-                    case 4:
-                        System.out.print("cat");
-                        break;
-                    case 5:
-                        System.out.print("child");
-                        break;
-                    case 6:
-                        System.out.print("bear");
-                        break;
-                    case 7:
-                        System.out.print("frog");
-                        break;
-                    case 8:
-                        System.out.print("horse");
-                        break;
-                    case 9:
-                        System.out.print("dog");
-                        break;
-                
-                } //ends switch
-            }//ends noun1
             
-            public static string verb(){
-                randomInt= randomGenerator.nextInt(10);
-                switch(randomInt){
-                    case 0:
-                        System.out.print("ran");
-                        break;
-                    case 1:
-                        System.out.print("walked");
-                        break;
-                    case 2:
-                        System.out.print("fell");
-                        break;
-                    case 3:
-                        System.out.print("jumped");
-                        break;
-                    case 4:
-                        System.out.print("skated");
-                        break;
-                    case 5:
-                        System.out.print("swam");
-                        break;
-                    case 6:
-                        System.out.print("biked");
-                        break;
-                    case 7:
-                        System.out.print("drove");
-                        break;
-                    case 8:
-                        System.out.print("flew");
-                        break;
-                    case 9:
-                        System.out.print("crawled");
-                        break;
-                
-                }
-             System.out.print(" to the ");  //articles that make the sentence work
-            }//ends verb
-            
-            public static string noun2{
-                randomInt= randomGenerator.nextInt(10);
-                switch(randomInt){
-                    case 0:
-                        System.out.print("table");
-                        break;
-                    case 1:
-                        System.out.print("door");
-                        break;
-                    case 2:
-                        System.out.print("chair");
-                        break;
-                    case 3:
-                        System.out.print("meeting");
-                        break;
-                    case 4:
-                        System.out.print("room");
-                        break;
-                    case 5:
-                        System.out.print("bar");
-                        break;
-                    case 6:
-                        System.out.print("game");
-                        break;
-                    case 7:
-                        System.out.print("show");
-                        break;
-                    case 8:
-                        System.out.print("car");
-                        break;
-                    case 9:
-                        System.out.print("beach");
-                        break;
-                
-                }
-            System.out.println(".");    
-            }//ends string noun2
         
-        System.out.print("If you would like another sentence press 1, if not press any other interger");
-        check=myscan.nextInt();
-        if(check=1){
-            continue=true;
+        System.out.println("If you would like another sentence press 1, if not press any other integer");
+        check=myScan.nextInt();
+        if(check==1){
+            cont=1;
         }
         else{
-            continue=false;
+            cont=0;
         }
             
             
